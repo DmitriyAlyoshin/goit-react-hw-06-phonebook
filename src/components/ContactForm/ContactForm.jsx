919ed4a5e881f6806b568eaa751bf6d9eeb700b6
Dmitriy Alyoshin <valerich1715@gmail.com>
2023-07-12 23:15:24 +0200

    Oy_vey_var_8

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -40,15 +40,15 @@ const schema = yup.object().shape({
     .trim()
     .matches(
       /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
-      'Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d`Artagnan'
+      'Name may contain only letters, apostrophe, dash and spaces. For example Dmytro, Sergiy Dykiy'
     )
     .required(),
   number: yup.string()
-    .phone(
-      'UA',
-      true,
-      'Phone number must be a valid phone number for region UA, digits and can contain spaces, dashes, parentheses and can start with +'
-    )
+    // .phone(
+    //   'EN',
+    //   true,
+    //   'Phone number must be a valid phone number for region UA, digits and can contain spaces, dashes, parentheses and can start with +'
+    // )
     .required(),
 });
 
